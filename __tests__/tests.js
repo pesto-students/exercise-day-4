d94f679f6d8774e6b07dabcae76a02c32feab3bd
Nishant <nishantsalhotra.ns@gmail.com>
2018-06-02 01:01:08 +0530

    Feat: Add more functional programming exercises

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -4,11 +4,16 @@ const {
   requestValidator,
   curry,
   memoize,
+  hundredThousandairs,
+  datasetWithRoundedDollar,
+  sumOfBankBalances,
+  sumOfInterests,
+  higherStateSums,
 } = require('../src');
 
 /* eslint-disable arrow-body-style   */
 describe('squareNumbersArray', () => {
-  test('should throw an error if array of numbers is not passes as argument', () => {
+  test('should throw an error if array of numbers is not passed as argument', () => {
     const myArray = [1, 2, 'string', 4];
     expect(() => squareNumbersArray(...myArray)).toThrow('My custom error');
   });
@@ -164,5 +169,55 @@ describe('memoize', () => {
     fib(10, 'x');
     fib(10, 'y');
     expect(called).toBe(22);
+  });
+});
+
+describe('hundredThousandairs', () => {
+  it('should return an array with accounts from bankBalances that are greater than 100000', () => {
+    expect(hundredThousandairs().length).toBe(93);
+    expect(hundredThousandairs().every(account => (account.amount > 100000))).toBe(true);
+  });
+});
+
+describe('datasetWithRoundedDollar', () => {
+  it('should return an array of accounts with an added key `rounded`', () => {
+    expect(datasetWithRoundedDollar().length)
+      .toBe(100);
+    expect(datasetWithRoundedDollar().every(account => Object.hasOwnProperty.call(account, 'rounded')))
+      .toBe(true);
+  });
+
+  it('each accounts `rounded` value should be rounded to the nearest dollar', () => {
+    expect(datasetWithRoundedDollar()[0].rounded)
+      .toBe(822371);
+    expect(datasetWithRoundedDollar()[7].rounded)
+      .toBe(231272);
+    expect(datasetWithRoundedDollar()[9].rounded)
+      .toBe(683737);
+    expect(datasetWithRoundedDollar()[10].rounded)
+      .toBe(913216);
+    expect(datasetWithRoundedDollar()[99].rounded)
+      .toBe(196086);
+  });
+});
+
+describe('sumOfBankBalances', () => {
+  it('should return the sum of all amounts in bankBalances, rounded to the nearest cent', () => {
+    expect(sumOfBankBalances())
+      .toBe(55502603.02);
+  });
+});
+
+describe('sumOfInterests', () => {
+  it('should return the sum the 18.9% interest for all amounts in bankBalances, in the selected states, rounded to the nearest cent', () => {
+    expect(sumOfInterests())
+      .toBe(2504611.23);
+  });
+});
+
+describe('higherStateSums', () => {
+  it('should return the sum of all amounts of every state, where the sum of amounts in the state is greater than 1,000,000', () => {
+    expect(higherStateSums())
+      .toBe(48629878.25);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-unused-vars */
+// NOTE: Do not use for or while loop, or Array.forEach in any of these questions
 
-// (*)
+const dataset = require('./dataset.json');
+
+// 1 (*)
 function squareNumbersArray(arr) {
   const squares = arr.map(number => number * number);
   return squares;
 }
 
-/**
+/** 2
  * Write a class for a checking account that validates it’s created with valid parameters.
  * A SavingsAccount has an accountNumber, email, firstName, lastName all set through the
  * constructor and an array of products that is initially empty.
@@ -18,7 +21,7 @@ class SavingsAccount {
 
 }
 
-/** (*)
+/** 3 (*)
  * Write a JS function that validates an HTTP request object.
  * The object has the properties method, uri, version and message.
  * Your function must receive the object as a parameter and verify
@@ -40,7 +43,7 @@ function requestValidator(obj) {
   return obj;
 }
 
-/*
+/* 4
 Create a function that caches another function
 Example:
 
@@ -60,29 +63,67 @@ expensiveOperation function is called!
 See 'memoize' tests for further info of the requirement
 */
 
-function memoize(f) {
-  const cache = {};
-  return (...args) => {
-    const argsString = JSON.stringify(args);
-    cache[argsString] = cache[argsString] || f(...args);
-    return cache[argsString];
-  };
-}
+function memoize() {}
 
-/*
+/* 5
 Here's the basic usage of the function that you'll be creating:
 
 function add(a, b) {
   return a + b;
 }
 
-var curriedAdd = curry(add); <- this is the curry function
+const curriedAdd = curry(add); <- this is the curry function
 console.log(  curriedAdd(1)(2)  ); // 3
 See 'curry' tests for further info of the requirement
 */
-function curry(fn) {
+function curry() {}
 
-}
+/* 6 (*)
+  Return an array with accounts from bankBalances that are
+  greater than 100000 without using for or while loop
+*/
+function hundredThousandairs() {}
+
+/* 7 (*)
+  DO NOT MUTATE DATA.
+
+  Return a new dataset where each bank object is a new object.
+  `amount` and `state` values will be transferred to the new object.
+  This new object is different, you will add one new key of `rounded`
+
+  `rounded` value is `amount` rounded to the nearest dollar
+
+  Example:
+    {
+      "amount": "134758.44",
+      "state": "HI",
+      "rounded": 134758
+    }
+*/
+function datasetWithRoundedDollar() {}
+
+// 8 (*)
+// Return the sum of all values held at `amount` for each bank object
+function sumOfBankBalances() {}
+
+/* 9 (*)
+  from each of the following states:
+    Wisconsin
+    Illinois
+    Wyoming
+    Ohio
+    Georgia
+    Delaware
+  take each `amount` and add 18.9% interest to it rounded to the nearest cent
+  and then sum it all up into one value saved to `sumOfInterests`
+ */
+function sumOfInterests() {}
+
+/* 10 (*)
+  Aggregate the sum of each state into one hash table
+  Return the sum of all states with totals greater than 1,000,000
+ */
+function higherStateSums() {}
 
 module.exports = {
   squareNumbersArray,
@@ -90,4 +131,9 @@ module.exports = {
   requestValidator,
   memoize,
   curry,
+  hundredThousandairs,
+  datasetWithRoundedDollar,
+  sumOfBankBalances,
+  sumOfInterests,
+  higherStateSums,
 };
