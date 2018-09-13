2e4e13be8823aad08dbb4bbd5c1ba34dfb53078d
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-09-13 23:41:23 +0530

    Fixed findElement

--- src/index.js
+++ src/index.js
@@ -25,7 +25,9 @@ function chunkArrayInGroups(arr, size) {
  * returns the first element in the array that passes a truth test (second argument)
  */
 function findElement(arr, func) {
+  for (let i = 0; i < arr.length; i += 1) if (func(arr[i])) return arr[i];
 
+  return undefined;
 }
 // findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
 
