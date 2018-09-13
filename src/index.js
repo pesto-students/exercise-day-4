a2840da922fa2b0260e7e7d6a270ef977611d1cc
Bharani <bharanidharan.nb@gmail.com>
2018-09-13 23:47:26 +0530

    findElement

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ function chunkArrayInGroups(arr, size) {
  * returns the first element in the array that passes a truth test (second argument)
  */
 function findElement(arr, func) {
-
+  return arr.find(func);
 }
 // findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
 
