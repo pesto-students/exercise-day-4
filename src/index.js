817a9cb690abba3876fadb82f11848143da0dcbc
Bharani <bharanidharan.nb@gmail.com>
2018-09-14 00:19:59 +0530

    Object invert, default

--- src/index.js
+++ src/index.js
@@ -149,6 +149,12 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const invObj = {};
+  // eslint-disable-next-line
+  for(const key in obj) {
+    invObj[obj[key]] = key;
+  }
+  return invObj;
 };
 
 // (*)
@@ -156,6 +162,14 @@ const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
+  const newObj = obj;
+  // eslint-disable-next-line
+  for(const key in defaultProps) {
+    if (!obj[key]) {
+      newObj[key] = defaultProps[key];
+    }
+  }
+  return newObj;
 };
 
 
