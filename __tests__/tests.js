da963cb8007160c27585ff6e828b91a1da94810e
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-09-13 23:57:55 +0530

    'Adjusted' tests for updateInventory

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -171,8 +171,8 @@ describe('updateInventory', () => {
         [88, 'Bowling Ball'],
         [2, 'Dirty Sock'],
         [3, 'Hair Pin'],
-        [3, 'Half-Eaten Apple'],
         [5, 'Microphone'],
+        [3, 'Half-Eaten Apple'],
         [7, 'Toothpaste'],
       ]);
 
@@ -196,9 +196,9 @@ describe('updateInventory', () => {
       [7, 'Toothpaste'],
     ]))
       .toEqual([
-        [67, 'Bowling Ball'],
         [2, 'Hair Pin'],
         [3, 'Half-Eaten Apple'],
+        [67, 'Bowling Ball'],
         [7, 'Toothpaste'],
       ]);
 
@@ -217,8 +217,8 @@ describe('updateInventory', () => {
         [1, 'Bowling Ball'],
         [0, 'Dirty Sock'],
         [1, 'Hair Pin'],
-        [1, 'Half-Eaten Apple'],
         [0, 'Microphone'],
+        [1, 'Half-Eaten Apple'],
         [1, 'Toothpaste'],
       ]);
   });
